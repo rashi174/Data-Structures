0c9287085c5ddbd9b422e6d8ef85b69012ef614d
Rashi Bansal <rashibansal79518@gmail.com>
2021-01-14 12:04:44 +0530

    Add Two Numbers Optimal Approach
    https://leetcode.com/problems/add-two-numbers/

--- Linked List/Add-Two-Numbers.py
+++ Linked List/Add-Two-Numbers.py
@@ -1,16 +1,11 @@
 '''
-2. Add Two Numbers
-Medium
 
-10439
+>> Time Complexity =  Max(len(l1),len(l2))
+>> Space Complexity = O(N)
 
-2560
+Add Two Numbers
 
-Add to List
-
-Share
 You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
-
 You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
  
